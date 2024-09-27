37e4168b771210a491eb0c834a3a1f4a4526905a
Gonzalo Peñaherrera E. <gonzalop67@gmail.com>
2024-09-27 04:11:40 -0500

    04 - Controladores en Laravel ¿Qué son y para que sirven? - Curso Laravel 10 desde cero.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\CursoController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,27 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    // return view('welcome');
-    return "Bienvenido a la página principal";
-});
+Route::get('/', HomeController::class);
 
-Route::get('cursos', function () {
-    return "Bienvenido a la página cursos";
-});
+Route::get('cursos', [CursoController::class, 'index']);
 
-Route::get('cursos/create', function () {
-    return "En esta página podrás crear un curso";
-});
+Route::get('cursos/create', [CursoController::class, 'create']);
 
-// Route::get('cursos/{curso}', function ($curso) {
-//     return "Bienvenido al curso: $curso";
-// });
+Route::get('cursos/{curso}', [CursoController::class, 'show']);
 
-Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
-    if ($categoria) {
-        return "Bienvenido al curso: $curso, de la categoria: $categoria";
-    } else {
-        return "Bienvenido al curso: $curso";
-    }
-});
