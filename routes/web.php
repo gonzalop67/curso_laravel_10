557ae6fc1d199f8705d2292e924f0b2f647d40ae
Gonzalo Peñaherrera E. <gonzalop67@gmail.com>
2024-09-26 21:13:43 -0500

    03 - Rutas en Laravel ¿Qué son y para que sirven? - Curso Laravel 10 desde cero.

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return "Bienvenido a la página principal";
+});
+
+Route::get('cursos', function () {
+    return "Bienvenido a la página cursos";
+});
+
+Route::get('cursos/create', function () {
+    return "En esta página podrás crear un curso";
+});
+
+// Route::get('cursos/{curso}', function ($curso) {
+//     return "Bienvenido al curso: $curso";
+// });
+
+Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+    if ($categoria) {
+        return "Bienvenido al curso: $curso, de la categoria: $categoria";
+    } else {
+        return "Bienvenido al curso: $curso";
+    }
 });
