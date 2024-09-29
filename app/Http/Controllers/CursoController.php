7b5d028bd38359d14be1708160bfabf299d7f777
Gonzalo Peñaherrera E. <gonzalop67@gmail.com>
2024-09-29 04:10:46 -0500

    19 - Qué son los Form Request - Curso Laravel 10 desde cero.

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCurso;
 use App\Models\Curso;
 use Illuminate\Http\Request;
 
@@ -28,14 +29,8 @@ class CursoController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCurso $request)
     {
-        $request->validate([
-            'name' => 'required|min:3',
-            'descripcion' => 'required',
-            'categoria' => 'required'
-        ]);
-
         $curso = new Curso();
 
         $curso->name = $request->name;
